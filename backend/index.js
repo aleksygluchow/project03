b1f48237a8fc2f2b2ba362a98ef751118868ebce
Aleksy Gluchow <aleksy.gluchow@gmail.com>
2025-06-24 16:35:44 +0200

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+// index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
