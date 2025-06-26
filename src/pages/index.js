60ba7f14fdab8d75d8e6fa7b5a1b76abf68f5cad
Aleksy Gluchow <aleksy.gluchow@gmail.com>
2025-06-26 19:40:33 +0200

    hi Herman

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1>Welcome to My Site</h1>
-      <p>Im a Physics Masters student interested in science, code, and creativity..</p>
+      <p>hi Herman..</p>
     </div>
   );
 }
