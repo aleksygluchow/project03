020a6d25e0c49a59f2e2f93ca28123e94bc8fb6e
Aleksy Gluchow <aleksy.gluchow@gmail.com>
2025-06-26 20:08:57 +0200

    favicons

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,23 @@
+import Head from 'next/head';
 import '../styles/globals.css';
 import Layout from '../components/Layout';
 
-export default function MyApp({ Component, pageProps }) {
+export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <title>Aliaksei Hlukhau – Portfolio & Blog</title>
+        <meta name="description" content="Physics, photonics, coding and creativity." />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Favicons and manifest */}
+        <link rel="icon" type="image/png" href="/favicon-32x32.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
