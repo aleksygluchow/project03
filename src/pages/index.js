0d08e6c4b117b837256d3305b957a1c71b24599b
Aleksy Gluchow <aleksy.gluchow@gmail.com>
2025-07-16 12:10:27 +0200

    adding to the cv

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,21 +5,28 @@ export default function Home() {
     <>
       <Head>
         <title>Home – Aliaksei Hlukhau</title>
+         <link
+    href="https://fonts.googleapis.com/css2?family=UnifrakturCook:wght@700&display=swap"
+    rel="stylesheet"
+  />
       </Head>
 
       <div>
         <h1>Welcome to My Site</h1>
-        <p>
-  Now in Germany{' '}
-  <img
-    src="https://flagcdn.com/w40/de.png"
-    alt="German flag"
-    width="20"
-    style={{ display: 'inline-block', verticalAlign: 'middle' }}
-  />
-
-
-  Hi Marcel
+        <p style={{ fontFamily: "'UnifrakturCook', cursive" }}>
+  Jetzt in Deutschland{' '}
+  <a
+    href="https://de.wikipedia.org/wiki/Deutschland"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <img
+      src="https://flagcdn.com/w40/de.png"
+      alt="Deutsche Flagge"
+      width="20"
+      style={{ display: 'inline-block', verticalAlign: 'middle', marginLeft: '4px' }}
+    />
+  </a>
 </p>
       </div>
     </>
